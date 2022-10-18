c1958ce434396d8ff7591ca0c351c4466eb30ba0
jochempunt <80535619+jochempunt@users.noreply.github.com>
2022-10-18 15:51:31 +0200

    added walk animation and  right left controll

--- MarioXS/Script/Build/Script.js
+++ MarioXS/Script/Build/Script.js
@@ -1,1 +1,108 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        constructor() {
+            super();
+            // Properties may be mutated by users in the editor via the automatically created user interface
+            this.message = "CustomComponentScript added to ";
+            // Activate the functions of this component as response to events
+            this.hndEvent = (_event) => {
+                switch (_event.type) {
+                    case "componentAdd" /* COMPONENT_ADD */:
+                        ƒ.Debug.log(this.message, this.node);
+                        break;
+                    case "componentRemove" /* COMPONENT_REMOVE */:
+                        this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                        this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                        break;
+                    case "nodeDeserialized" /* NODE_DESERIALIZED */:
+                        // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                        break;
+                }
+            };
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* NODE_DESERIALIZED */, this.hndEvent);
+        }
+    }
+    // Register the script as component for use in the editor via drag&drop
+    CustomComponentScript.iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    var ƒAid = FudgeAid;
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    document.addEventListener("interactiveViewportStarted", start);
+    let marioTransformNode;
+    let marioNode;
+    let spriteNode;
+    function start(_event) {
+        viewport = _event.detail;
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        //ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        let branch = viewport.getBranch();
+        marioTransformNode = branch.getChildrenByName("MarioTransform")[0];
+        marioNode = marioTransformNode.getChildrenByName("Mario")[0];
+        hndLoad();
+    }
+    async function hndLoad() {
+        //let root: ƒ.Node = new ƒ.Node("root");
+        let imgSpriteSheet = new ƒ.TextureImage();
+        await imgSpriteSheet.load("./images/mario_walk2.png");
+        let coat = new ƒ.CoatTextured(undefined, imgSpriteSheet);
+        let animation = new ƒAid.SpriteSheetAnimation("Walk", coat);
+        animation.generateByGrid(ƒ.Rectangle.GET(3, 0, 17, 33), 4, 11, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(17));
+        //todo jump
+        spriteNode = new ƒAid.NodeSprite;
+        spriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+        spriteNode.setAnimation(animation);
+        spriteNode.setFrameDirection(1);
+        spriteNode.mtxLocal.translateY(-1);
+        spriteNode.framerate = 12;
+        marioTransformNode.removeAllChildren();
+        marioTransformNode.appendChild(spriteNode);
+        //root.addChild(spriteNode);
+        // setup viewport
+        marioTransformNode.getComponent(ƒ.ComponentTransform).mtxLocal.scaleX(0.5);
+        marioTransformNode.getComponent(ƒ.ComponentTransform).mtxLocal.scaleY(0.5);
+        //viewport.draw();
+        //ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 30);
+        //document.forms[0].addEventListener("change", handleChange);
+    }
+    let directionRight = true;
+    function update(_event) {
+        // ƒ.Physics.simulate();  // if physics is included and used
+        console.log("update");
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D])) {
+            marioTransformNode.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.1);
+            if (!directionRight) {
+                spriteNode.getComponent(ƒ.ComponentTransform).mtxLocal.rotateY(180);
+                directionRight = true;
+            }
+        }
+        else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A])) {
+            marioTransformNode.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(-0.1);
+            if (directionRight) {
+                spriteNode.getComponent(ƒ.ComponentTransform).mtxLocal.rotateY(180);
+                directionRight = false;
+            }
+        }
+        else {
+            spriteNode.showFrame(1);
+        }
+        viewport.draw();
+        ƒ.AudioManager.default.update();
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
--- MarioXS/Script/Source/Main.ts
+++ MarioXS/Script/Source/Main.ts
@@ -1,19 +1,106 @@
 namespace Script {
   import ƒ = FudgeCore;
+  import ƒAid = FudgeAid;
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
-  function start(_event: CustomEvent): void {
+  let marioTransformNode :ƒ.Node ;
+  let marioNode : ƒ.Node; 
+  let spriteNode: ƒAid.NodeSprite;
+
+  function start(_event: CustomEvent):void {
     viewport = _event.detail;
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    //ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    
+    
+    
+    
+    let branch: ƒ.Node = viewport.getBranch();
+    marioTransformNode =  branch.getChildrenByName("MarioTransform")[0];
+    
+    marioNode = marioTransformNode.getChildrenByName("Mario")[0];
+
+    hndLoad();
+    
+ 
   }
+
+
+  async function hndLoad(): Promise<void> {
+    //let root: ƒ.Node = new ƒ.Node("root");
+
+    let imgSpriteSheet: ƒ.TextureImage = new ƒ.TextureImage();
+    await imgSpriteSheet.load("./images/mario_walk2.png");
+    let coat: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, imgSpriteSheet);
+
+    let animation: ƒAid.SpriteSheetAnimation = new ƒAid.SpriteSheetAnimation("Walk", coat);
+    animation.generateByGrid(ƒ.Rectangle.GET(3, 0, 17, 33), 4, 11, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(17));
+    
+    //todo jump
+
+    spriteNode = new ƒAid.NodeSprite ;
+    spriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+    spriteNode.setAnimation(animation);
+    spriteNode.setFrameDirection(1);
+    spriteNode.mtxLocal.translateY(-1);
+    spriteNode.framerate = 12;
+
+    
+    marioTransformNode.removeAllChildren();
+    marioTransformNode.appendChild(spriteNode);
+  
+    //root.addChild(spriteNode);
+
+
+
+    // setup viewport
+    marioTransformNode.getComponent(ƒ.ComponentTransform).mtxLocal.scaleX(0.5);
+    marioTransformNode.getComponent(ƒ.ComponentTransform).mtxLocal.scaleY(0.5);
+    
+    //viewport.draw();
+
+    //ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 30);
+
+    //document.forms[0].addEventListener("change", handleChange);
+  }
+ 
+
+
+
+
+  let directionRight:boolean = true;
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
+    
+    console.log("update");
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D])){
+      marioTransformNode.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.1);
+      if(!directionRight){
+        spriteNode.getComponent(ƒ.ComponentTransform).mtxLocal.rotateY(180);
+        directionRight = true;
+      }
+
+
+    }else if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A])){
+      
+      marioTransformNode.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(-0.1);
+      if(directionRight){
+        spriteNode.getComponent(ƒ.ComponentTransform).mtxLocal.rotateY(180);
+        directionRight = false;
+      }
+      
+    }
+    else{
+      spriteNode.showFrame(1);
+    }
+    
+
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
